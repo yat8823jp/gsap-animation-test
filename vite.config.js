bac5e95a4ed5c1919d7c2b68e16f1242e773e42e
YAT <yat@yat-net.com>
2022-12-27 21:18:34 +0900

    add-glob-path-tailwindcss-autoprefixer

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,46 @@
 import { defineConfig } from "vite";
 import postcssNesting from "postcss-nesting";
-import sassGlobImports from 'vite-plugin-sass-glob-import';
+// import sassGlobImports from "vite-plugin-sass-glob-import";
+import glob from "glob";
+import path from "path";
 
 export default defineConfig({
+	// plugins: [
+	// 	sassGlobImports,
+	// ],
 	css: {
 		postcss: {
 			plugins: [
 				postcssNesting,
-				sassGlobImports
+				require( "tailwindcss" ),
+				require( "autoprefixer" )
+				// sassGlobImports,
 			],
 		},
 	},
 	build: {
+		outDir: `dist`,
+		rollupoptions: {
+			input: object.fromEntries (
+				glob
+					.sync( "{ js, css }/**/*.{ js, scss }", {
+						ignore: "**/_**/**/*.{ js, scss }",
+						cwd: `./src`,
+					} )
+					.map( ( file ) => {
+						const { dir, name } = path.parse( file );
+						return [ `${ dir }/${ name }`, path.resolve( "src", file ) ];
+					} )
+			),
+			output: {
+				entryFileNames: `assets/[name].js`,
+				assetFileNames: `assets/[name].[ext]`,
+			},
+		},
 		lib: {
 			entry: './lib/main.js',
 			name: 'Counter',
 			fileName: 'counter'
 		}
 	}
-})
+});
